c75fddb1e7bc1f0b831dcf49d3cc48c3be84b90f
Brayan <brayansandoval556@gmail.com>
2023-06-23 11:39:35 -0500

    diagrama clase busqueda brayan2

---
+++ Busqueda/busqueda.py
@@ -0,0 +1,16 @@
+class busqueda:
+    def __init__(self,buscar):
+        self.buscar = buscar 
+
+def buscar_oferta():
+    pass
+def mostrar_sugerencias():
+    pass
+def filtrar_ciudad():
+    pass
+def filtrar_departamento():
+    pass
+def mostrar_cargo():
+    pass
+def mostrar_vacante():
+    pass
