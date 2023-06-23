9bc4bf0494e70102bf3cb1e65733896fef7a6918
Brayan <brayansandoval556@gmail.com>
2023-06-23 11:40:40 -0500

    diagrama clase busqueda brayan2

---
+++ Busqueda/persona.py
@@ -0,0 +1,39 @@
+class persona:
+    def __init__(self,persona,credenciales,infoLaboral,infoPersonal,hoja_vida):
+        self.persona = persona
+        self.credenciales = credenciales
+        self.infoLaboral =infoLaboral
+        self.infoPersonal = infoPersonal
+        self.hoja_vida = hoja_vida
+
+    def ver_ficha():
+        pass
+    def postularse():
+        pass
+    def ver_Ponderacion():
+        pass
+
+def setnombre(self,nombre):
+    self.nombre = nombre
+def getnombre(self):
+    return self.nombre
+
+def setcredenciales(self,credenciales):
+    self.credenciales = credenciales
+def getcredenciales(self):
+    return self.credenciales 
+
+def setinfoLaboral(self, infoLaboral):
+    self.infoLaboral=infoLaboral
+def getinfoLaboral(self,):
+    return self.infoLaboral
+
+def setinfoPersonal(self, infoPersonal):
+    self.infoPersonal=infoPersonal
+def getinfoPersonal(self,):
+    return self.infoLaboral
+
+def sethojaVida(self,hojaVida):
+    self.hojaVida=hojaVida
+def gethojaVida(self,):
+    return self.hojaVida
