8f6269a322623675fcfa1c4c813f713b6672135a
andres <sairmontoya11@gmail.com>
2023-06-23 11:52:36 -0500

    Trabajo Terminar

---
+++ ClassPostulacion_Andres/Persona.py
@@ -0,0 +1,34 @@
+class Persona:
+    def __init__(self,nombre,Id_persona,correo,num_telefono):
+        self.nombre = nombre
+        self.id_persona = Id_persona
+        self.correo = correo
+        self.num_telefono = num_telefono
+
+def setNombre (self, nombre):
+    self.__nombre=nombre
+def get_Nombre(self):
+    return self.nombre
+
+def setId_persona (self,Id_persona):
+    self.__Id_persona=Id_persona
+def getId_persona(self):
+    return self.Id_persona
+
+def setCorreo (self,correo):
+    self.__correo=correo
+def getCorreo (self):
+    return self.correo
+
+def set_num_telefono (self,num_telefono):
+    self.__num_telefono=num_telefono
+def get_num_telefono (self):
+    return self.num_telefono
+
+def Datos(self):
+    if self.__nombre =="Andres" and self.__Id_persona == 10123 and self.__corrreo == "Sairmontoya11@gmail.com" and self.__num_telefono == 3204038801:
+        print("La informacion del usuario es correcta")
+    else:
+        print("la informacion del ususario es inconrrecta")
+
+
