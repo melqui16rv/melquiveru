8897f627d5373446d7a5ac6cecf73320046f37ac
SairMontoya <131676347+SairMontoya@users.noreply.github.com>
2023-06-25 19:24:53 -0500

    Clase empresa

---
+++ ClassPostulacion_Andres/Empresa.py
@@ -0,0 +1,34 @@
+Class Empresa:
+
+def __init__(self,nombre,Id_Empresa,RUT):
+        self.nombre = nombre
+        self.Id_Empresa = Id_Empresa
+        self.RUT = RUT
+       
+
+def setNombre (self, nombre):
+    self.__nombre=nombre
+def get_Nombre(self):
+    return self.__nombre
+
+def setId_persona (self,Id_Empresa):
+    self.__Id_Empresa = Id_Empresa
+def getId_persona(self):
+    return self.__Id_Empresa
+
+def setRUT(self,RUT):
+    self.__RUT= RUT
+def getRUT (self):
+    return self.__RUT
+
+def Publicar_cargo (self):
+    Publicar_cargo = input("ingrese el cargo que quiere publicar")
+
+def seleccionar_individuo (self):
+    seleccionar_individuo = input ("seleccione el individuo")
+
+def cerrar_cargo (self):
+    print("Se cierra el cargo")
+ 
+
+       
